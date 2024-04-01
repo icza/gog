d4c28ea6e44f81b18b47e5cb0b443aa540be018c
Andras Belicza <iczaaa@gmail.com>
2024-04-01 21:30:45 +0200

    Return slices of the same type as the inputs

--- slicesx/slicesx.go
+++ slicesx/slicesx.go
@@ -4,18 +4,18 @@ Package slicesx provides generic slice utility functions.
 package slicesx
 
 // Props returns a slice of a property, which is accessed using the given getter.
-func Props[V, P any](c []V, getter func(v V) P) []P {
-	props := make([]P, len(c))
-	for i, v := range c {
+func Props[E, P any](vals []E, getter func(v E) P) []P {
+	props := make([]P, len(vals))
+	for i, v := range vals {
 		props[i] = getter(v)
 	}
 	return props
 }
 
 // PropMap returns a map, mapping from a property, which is accessed using the given getter.
-func PropMap[V any, P comparable](c []V, getter func(v V) P) map[P]V {
-	m := make(map[P]V, len(c))
-	for _, v := range c {
+func PropMap[E any, P comparable](vals []E, getter func(v E) P) map[P]E {
+	m := make(map[P]E, len(vals))
+	for _, v := range vals {
 		m[getter(v)] = v
 	}
 	return m
@@ -23,9 +23,9 @@ func PropMap[V any, P comparable](c []V, getter func(v V) P) map[P]V {
 
 // PropsMap returns a map, mapping from a property, which is accessed using the given getter.
 // It is allowed / normal that multiple elements have the same property value, so map values are slices collecting those elements.
-func PropsMap[V any, P comparable](c []V, getter func(v V) P) map[P][]V {
-	m := make(map[P][]V)
-	for _, v := range c {
+func PropsMap[S ~[]E, E any, P comparable](vals S, getter func(v E) P) map[P]S {
+	m := make(map[P]S)
+	for _, v := range vals {
 		p := getter(v)
 		m[p] = append(m[p], v)
 	}
@@ -33,9 +33,9 @@ func PropsMap[V any, P comparable](c []V, getter func(v V) P) map[P][]V {
 }
 
 // Filter returns a new slice holding only the filtered elements.
-func Filter[V any](c []V, f func(v V) bool) []V {
-	var out []V
-	for _, v := range c {
+func Filter[S ~[]E, E any](vals S, f func(v E) bool) S {
+	var out S
+	for _, v := range vals {
 		if f(v) {
 			out = append(out, v)
 		}
