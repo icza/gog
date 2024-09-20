07715264eaa14674ecc1fd58dd27afe7b1473afe
Andras Belicza <iczaaa@gmail.com>
2024-09-20 09:33:59 +0200

    Add OpCache.MultiGet()

--- opcache.go
+++ opcache.go
@@ -3,6 +3,7 @@ package gog
 import (
 	"context"
 	"crypto/sha1"
+	"slices"
 	"sync"
 	"time"
 )
@@ -51,9 +52,9 @@ type OpCacheConfig struct {
 
 // OpCache implements a general value cache.
 // It can be used to cache results of arbitrary operations.
-// Cached values are tied to a string key that should be derived from the operation's parameters.
+// Cached values are tied to a string key that should be derived from the operation's arguments.
 // Cached values have an expiration time and also a grace period during which the cached value
-// is considered valid, but getting a cached value during the grace period triggers a reload
+// is considered usable, but getting a cached value during the grace period triggers a reload
 // that will happen in the background (the cached value is returned immediately, without waiting).
 //
 // Operations are captured by a function that returns a value of a certain type (T) and an error.
@@ -106,7 +107,7 @@ func (oc *OpCache[T]) Evict() {
 // execOp() is called in the background to refresh the cache,
 // and the cached result is returned immediately.
 // Care is taken to only launch a single background worker to refresh the cache even if
-// Get() is called multiple times with the same key before the cache can be refreshed.
+// Get() or MultiGet() is called multiple times with the same key before the cache can be refreshed.
 //
 // Else result is either not cached or we're past even the grace period:
 // execOp() is executed, the function waits for its return values, the result is cached,
@@ -123,7 +124,7 @@ func (oc *OpCache[T]) Get(
 		return cachedResult.result, cachedResult.resultErr
 	}
 
-	// This function executes execOp(), caches the result according to the configuration, and returns it
+	// execOpAndCache executes execOp(), caches the result according to the configuration, and returns it
 	execOpAndCache := func() (result T, resultErr error) {
 		result, resultErr = execOp()
 		expiration, graceExpiration := oc.cfg.ResultExpiration, oc.cfg.ResultGraceExpiration
@@ -176,6 +177,135 @@ func (oc *OpCache[T]) Get(
 	// reload in new goroutine.
 	// Note: we're not using the return values, we're returning the cached (grace-valid) values.
 	go execOpAndCache()
+
+	return
+}
+
+// MultiGet gets the results of a multi-operation.
+// A multi-operation is an operation that accepts a slice of keys, and can produce results for multiple input parameters
+// more efficiently than calling the operation for each input separately.
+//
+// results and resultErrs will be slices with identical size and elements matching to that of keys.
+//
+// Each result is taken from the cache if present and valid, or we're within its grace period.
+// If there are entries that are either not cached or we're past their grace period,
+// execMultiOp() is executed for those keys, the function waits for its return values, the results are cached,
+// and the fresh results are returned.
+//
+// If there are results that are returned because they are cached but not valid but we're within the grace period,
+// execMultiOp() is called in the background to refresh them. Care is taken to only launch a single background worker
+// to refresh each such entry even if Get() or MultiGet() is called multiple times with the same key(s)
+// before the cache can be refreshed.
+//
+// execMultiOp must return results and errs slices with identical size to that of its keyIndices argument,
+// and elements matching to keys designated by keyIndices! Failure to do so is undefined behavior,
+// may even result in runtime panic!
+//
+// The implementation may alter key values (by transforming / simplifying them).
+// Since execMultiOp() may be called later, in the background, the caller must not modify the keys slice.
+func (oc *OpCache[T]) MultiGet(
+	keys []string,
+	execMultiOp func(keyIndices []int) (results []T, errs []error),
+) (results []T, resultErrs []error) {
+
+	// Let's "detach" keys slice from the caller: make our own copy in which we may also modify them (transform):
+	keys = slices.Clone(keys)
+
+	results = make([]T, len(keys))
+	resultErrs = make([]error, len(keys))
+	cachedResults := make([]*opResult[T], len(keys))
+
+	var (
+		invalidKeyIndices    []int // key indices that we must produce and wait for
+		graceValidKeyIndices []int // key indices that we may use but must refresh in the background
+	)
+
+	for keyIdx, key := range keys {
+		keys[keyIdx] = transformKey(key)
+
+		cachedResult := oc.getCachedOpResult(key)
+
+		switch {
+		case cachedResult.valid():
+			results[keyIdx], resultErrs[keyIdx] = cachedResult.result, cachedResult.resultErr
+		case cachedResult.graceValid():
+			// Cached result is within grace period, we can use it:
+			results[keyIdx], resultErrs[keyIdx] = cachedResult.result, cachedResult.resultErr
+			graceValidKeyIndices = append(graceValidKeyIndices, keyIdx)
+			cachedResults[keyIdx] = cachedResult
+		default:
+			// Not valid and not even within grace period: query, cache and return:
+			invalidKeyIndices = append(invalidKeyIndices, keyIdx)
+		}
+	}
+
+	// execMultiOpAndCache executes execMultiOp(), caches the results according to the configuration, and returns them
+	execMultiOpAndCache := func(keyIndices []int) (results []T, resultErrs []error) {
+		results, resultErrs = execMultiOp(keyIndices)
+		for i, resultErr := range resultErrs {
+			expiration, graceExpiration := oc.cfg.ResultExpiration, oc.cfg.ResultGraceExpiration
+			if resultErr != nil && oc.cfg.ErrorExpiration != nil {
+				discard, exp, graceExp := oc.cfg.ErrorExpiration(resultErr)
+				if discard {
+					// This error result is not to be cached at all, just skip:
+					continue
+				}
+				if exp != nil {
+					expiration = *exp
+				}
+				if graceExp != nil {
+					graceExpiration = *graceExp
+				}
+			}
+			oc.setCachedOpResult(keys[keyIndices[i]], newOpResult(results[i], resultErr, expiration, graceExpiration))
+		}
+		return
+	}
+
+	if len(invalidKeyIndices) > 0 {
+		// Call execMultiOpAndCache and wait for its results!
+		mresults, mresultErrs := execMultiOpAndCache(invalidKeyIndices)
+		for i, result := range mresults {
+			keyIdx := invalidKeyIndices[i]
+			results[keyIdx], resultErrs[keyIdx] = result, mresultErrs[i]
+		}
+	}
+
+	if len(graceValidKeyIndices) > 0 {
+		// Launch background goroutine in which call execMultiOp (we're not waiting for its results)!
+
+		// First let's see which elements we do need to process, and if we're the one to do it:
+		graceValidKeyIndices2 := make([]int, 0, len(graceValidKeyIndices))
+		for _, keyIdx := range graceValidKeyIndices {
+			cachedResult := cachedResults[keyIdx]
+
+			// First use read-lock to check if someone's already doing it:
+
+			cachedResult.reloadMu.RLock()
+			reloading := cachedResult.reloading
+			cachedResult.reloadMu.RUnlock()
+			if reloading {
+				// Already reloading, nothing to do
+				continue
+			}
+
+			// Try to take ownership of reloading, needs write-lock:
+			cachedResult.reloadMu.Lock()
+			if cachedResult.reloading {
+				// Someone else got the write-lock first, he'll take care of the reload
+				cachedResult.reloadMu.Unlock()
+				continue
+			}
+			cachedResult.reloading = true // We'll be the one to do it
+			cachedResult.reloadMu.Unlock()
+			graceValidKeyIndices2 = append(graceValidKeyIndices2, keyIdx)
+		}
+		if len(graceValidKeyIndices2) > 0 {
+			// reload in new goroutine.
+			// Note: we're not using the return values, we're returning the cached (grace-valid) values.
+			go execMultiOpAndCache(graceValidKeyIndices2)
+		}
+	}
 
 	return
 }
